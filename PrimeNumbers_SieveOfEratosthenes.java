246a5acfc83ab30ca3afdb6c5e355fa333e19571
FenikkusuKoneko <fenikkusukoneko@gmail.com>
2020-10-01 23:45:10 +0300

    Added calculating prime numbers using the Sieve of Eratosthenes algorithm

---
+++ PrimeNumbers_SieveOfEratosthenes.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Please enter a maximum number: ");
+
+        var n = scanner.nextInt();
+        System.out.printf("Calculating the prime numbers between 2 and %d\n", n);
+
+        var sieve = new SieveOfEratosthenes(n);
+        sieve.CalculatePrimeNumbers();
+    }
+}
+
+public class SieveOfEratosthenes {
+
+    private final List<Number> numbers;
+    private final List<Integer> primeNumbers;
+
+    public SieveOfEratosthenes(int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("n cannot be lower than 2");
+        }
+
+        numbers = new ArrayList<>(n - 1);
+        for (var i = 2; i <= n; i++)
+            numbers.add(new Number(i));
+
+        primeNumbers = new ArrayList<>();
+    }
+
+    public void CalculatePrimeNumbers() {
+        for (var i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i).isChecked())
+                continue;
+
+            var number = numbers.get(i).getNumber();
+            primeNumbers.add(number);
+
+            for (var n : numbers) {
+                if (n.isChecked())
+                    continue;
+
+                if (n.getNumber() % number == 0)
+                    n.setChecked(true);
+            }
+        }
+
+        for (var primeNumber : primeNumbers) {
+            System.out.println(primeNumber);
+        }
+    }
+
+    private class Number {
+        private final int number;
+        private boolean checked;
+
+        public Number(int number) {
+            this.number = number;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public boolean isChecked() {
+            return checked;
+        }
+
+        public void setChecked(boolean checked) {
+            this.checked = checked;
+        }
+    }
+}
