30fadd3cf019aeeea9727ffb35c141ffb4cba750
nipungera47 <60498013+nipungera47@users.noreply.github.com>
2020-10-01 19:23:34 +0530

    Add files via upload

---
+++ demo83.java
@@ -0,0 +1,16 @@
+//java program to demonstrate accessing of the members 
+//when package is not imported 
+public class demo83{
+	public demo83(){
+		java.util.ArrayList newList= new java.util.ArrayList();
+		
+		// we have made the object here names New list
+		// here we have not imported the package
+		//But we are refering to it
+		
+	}
+	public static void main(String args[])
+	{
+		// Here is the code of main class.
+	}
+}
