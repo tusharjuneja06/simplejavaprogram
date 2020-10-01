5aab4fdb2c3f9e063bbd5e52264e9f91506ee70e
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-01 20:23:08 +0530

    Updated code

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,7 +13,7 @@ public class BubbleSort {
 	// Function to print elements
 	void printArray(int arr[]) {
 		for (int i = 0; i < arr.length; ++i)
-			System.out.print(arr[i] + " ");
+			System.out.println(arr[i] + " ");
 		System.out.println();
 	}
 
