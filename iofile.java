53bdcdf6fd28a58f63611edf56b9e91a7259fe5f
Ritwik <singhritwik2710@gmail.com>
2020-10-02 00:20:51 +0530

    added input method for array

--- iofile.java
+++ iofile.java
@@ -9,6 +9,8 @@ public class IO {
         double c;
         char d;
         String e;
+        int arrsize;
+        int [] arr;
 
         System.out.println("Enter the string");
         e = sc.nextLine();
@@ -24,12 +26,22 @@ public class IO {
 
         System.out.println("Enter the char");
         d = sc.next().charAt(0);
+        
+        System.out.println("Enter size of 1-d array");
+        arrsize=sc.nextInt();
+        System.out.println("Enter values for array");
+        for(int size=0;size<arrsize;size++) {
+        	arr[size]=scn.nextInt();
+        }
 
         System.out.println("string value: " + e);
         System.out.println("byte value: " + a);
         System.out.println("int value: " + b);
         System.out.println("double value: " + c);
         System.out.println("char value: " + d);
+        System.out.println("array value: ");
+        for(int val:arr)
+        	System.out.print(val+" ");
         sc.close();
     }
 }
