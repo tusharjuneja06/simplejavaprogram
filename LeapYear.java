d2e587eb99dcca364be638139c7d052437340715
Akashkpdroid <71496133+Akashkpdroid@users.noreply.github.com>
2020-10-08 07:56:43 +0530

    Modified leap year checking

--- LeapYear.java
+++ LeapYear.java
@@ -3,29 +3,17 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);  
+        System.out.print("Enter The year :  ");  
         System.out.println("Enter The year :  ");  
         int year =  sc.nextInt();
         boolean leap = false;
 
-        if(year % 4 == 0)
-        {
-            if( year % 100 == 0)
-            {
-                // year is divisible by 400, hence the year is a leap year
-                if ( year % 400 == 0)
-                    leap = true;
-                else
-                    leap = false;
-            }
-            else
-                leap = true;
-        }
-        else
-            leap = false;
-
+        if((year%400==0) ||(year%4==0&&year%100!=0))//checking for leap year
+             leap=true;
         if(leap)
             System.out.println(year + " is a leap year.");
         else
             System.out.println(year + " is not a leap year.");
     }
-}
+} 
+
