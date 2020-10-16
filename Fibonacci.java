41a1a91f4a1915af2bc61db0424558efe762a50b
Avi Parmar <aviparmar326@gmail.com>
2020-10-17 01:59:15 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,23 +1,29 @@
-import java.util.*;  
-public class Fibonacci {
-
-    public static void main(String[] args) {
-
-	 int n , t1 = 0, t2 = 1;
-        System.out.println("Please Input your number to print Fibonacci Series");
-
-        Scanner sc= new Scanner(System.in);   
-        n= sc.nextInt();  
-        System.out.println("First " + n + " terms: ");
-
-
-        for (int i = 1; i <= n; ++i)
-        {
-            System.out.println(t1 + " ");
-
-            int sum = t1 + t2;
-            t1 = t2;
-            t2 = sum;
-        }
-    }
-}
+// Fibonacci Series using Dynamic Programming 
+class fibonacci 
+{ 
+   static int fib(int n) 
+    { 
+    /* Declare an array to store Fibonacci numbers. */
+    int f[] = new int[n+2]; // 1 extra to handle case, n = 0 
+    int i; 
+       
+    /* 0th and 1st number of the series are 0 and 1*/
+    f[0] = 0; 
+    f[1] = 1; 
+      
+    for (i = 2; i <= n; i++) 
+    { 
+       /* Add the previous 2 numbers in the series 
+         and store it */
+        f[i] = f[i-1] + f[i-2]; 
+    } 
+       
+    return f[n]; 
+    } 
+       
+    public static void main (String args[]) 
+    { 
+        int n = 9; 
+        System.out.println(fib(n)); 
+    } 
+} 
